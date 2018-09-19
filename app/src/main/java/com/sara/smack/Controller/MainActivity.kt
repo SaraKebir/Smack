d52f11f17999503dbca14384a4fe892d81d48e03
saraKebir <fs_kebir@esi.dz>
2018-09-19 18:49:46 +0100

    Initial commit

---
+++ app/src/main/java/com/sara/smack/Controller/MainActivity.kt
@@ -0,0 +1,82 @@
+package com.sara.smack.Controller
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import android.graphics.Color
+import android.os.Bundle
+import android.support.v4.content.LocalBroadcastManager
+import android.support.v4.view.GravityCompat
+import android.support.v7.app.ActionBarDrawerToggle
+import android.support.v7.app.AppCompatActivity
+import android.view.View
+import com.sara.smack.R
+import com.sara.smack.Services.AuthService
+import com.sara.smack.Services.UserDataService
+import com.sara.smack.Utilities.BROADCAST_USER_DATA_CHANGE
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.app_bar_main.*
+import kotlinx.android.synthetic.main.nav_header_main.*
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+        val toggle = ActionBarDrawerToggle(
+                this, drawer_layout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
+        drawer_layout.addDrawerListener(toggle)
+        toggle.syncState()
+        LocalBroadcastManager.getInstance(this).registerReceiver(userDataChangeReceiver, IntentFilter(BROADCAST_USER_DATA_CHANGE))
+
+    }
+
+    private val userDataChangeReceiver= object:BroadcastReceiver(){
+        override fun onReceive(context: Context?, intent: Intent?) {
+            if(AuthService.isLoggedIn){
+                userNameNavHeader.text=UserDataService.name
+                userEmailNavHeader.text=UserDataService.email
+                val resourceId=resources.getIdentifier(UserDataService.avatarName,"drawable",packageName)
+                userImageNavHeader.setImageResource(resourceId)
+                //userImageNavHeader.setBackgroundColor(UserDataService.returnAvatarColor(UserDataService.avatarColor))
+                loginBtnNavHeader.text="logout"
+
+
+            }
+
+        }
+    }
+
+    override fun onBackPressed() {
+        if (drawer_layout.isDrawerOpen(GravityCompat.START)) {
+            drawer_layout.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
+        }
+    }
+
+    fun loginBtnNavClicked(view:View){
+        if(AuthService.isLoggedIn){
+            UserDataService.logout()
+            userNameNavHeader.text="Login"
+            userEmailNavHeader.text=""
+            userImageNavHeader.setImageResource(R.drawable.profiledefault)
+            userImageNavHeader.setBackgroundColor(Color.TRANSPARENT)
+            loginBtnNavHeader.text="login"
+
+        }else{
+            val loginIntent =Intent(this, LoginActivity::class.java)
+            startActivity(loginIntent)
+        }
+
+    }
+    fun addChannelClicked(view: View){
+
+    }
+    fun sendMsgBtnClicked(view: View){
+
+    }
+}
